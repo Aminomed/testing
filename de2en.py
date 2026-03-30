0bad705d2a187a939510ec45fbeee58423ff1eb6
Amin <3015165@stud.th-mannheim.de>
2026-03-30 16:29:35 +0200

    fixed und debugger proof

--- de2en.py
+++ de2en.py
@@ -1,18 +1,18 @@
 import sys
 
 de2en = {
-    1: "one",
-    2: "two",
-    3: "three",
+    "1": "one",
+    "2": "two",
+    "3": "three",
     "eins": "one",
     "zwei": "two",
     "drei": "three"
 }
 
-def main(args):
-    n = args[0]
-    print(n + " auf englisch ist ") 
-    print(de2en[n])
+def main(args): 
+    n = args[0] if args else "1"
+    print(n , " auf englisch ist ") 
+    print(de2en[n]) 
 
 if __name__ == '__main__':
     main(sys.argv[1:])
