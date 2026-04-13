e5369edb7f9c5b611a524cf970da5da55d9f0080
Amin <3015165@stud.th-mannheim.de>
2026-04-13 15:25:05 +0200

    kritzelei

--- kritzelei.py
+++ kritzelei.py
@@ -1,12 +1,13 @@
 import sys
 
 def main():
-    lis = [1, 2, 3]
-    lis[len(lis):] = [4]
-    unter = [5, 6]
-    lis[1] = unter
-    unter[0] = 7
-    print(lis)
+
+    
+    dic = {1: "eins", 2: "zwei", 3: "drei"}
+
+    for key in dic:
+       print(key, dic[key])
+    
 
 if __name__ == '__main__':
     main()
